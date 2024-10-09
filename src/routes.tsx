06832f2de60b90deda19a37b3448535d50bc1abe
EzequielC-Dev <ezequiel34324l@gmail.com>
2024-10-09 07:43:57 -0300

    Criação da página de cadastro e de login para facilitar a experiência do usuário

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./services/login";
+import Cadastro from "./services/cadastro";
 import Layout from "./components/global/Layout";
 
 function RoutesApp() {
@@ -11,6 +12,7 @@ function RoutesApp() {
           <Route index element={<Home />} />
           <Route path="*" index element={<h1>404 Error Not Found</h1>} />
           <Route path="/login" index element={<Login />} />
+          <Route path="/cadastro" index element={<Cadastro></Cadastro>}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
